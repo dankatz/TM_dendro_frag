b62c73ca06f2e3b28e633625f5ab61e92d30f67f
Daniel Katz <dankatz512@gmail.com>
2016-02-12 12:14:33 -0500

    fixed simulation so it pulls from the offset data

--- datasim_coreoffset.R
+++ datasim_coreoffset.R
@@ -39,7 +39,7 @@ g <- matrix(NA, nrow = 100, ncol = 30)
 
 for(i in 1:30){
   for(j in 1:100){
-    g[j,i] <- w[j] * 0.5 + rnorm(n = 1, mean =0, sd = 0.5)
+    g[j,i] <- w[j] * 1 + rnorm(n = 1, mean =0, sd = 0.5)
   }
 }
 
@@ -53,19 +53,21 @@ ggplot(wgy, aes(x = y, y = growth, color = as.factor(tree))) + geom_point()
 ggplot(wgy, aes(x = w, y = growth, color = as.factor(tree))) +geom_point() + geom_smooth(method = "lm", se = FALSE)
 
 #offset individual trees
-for(i in 1:5){wgy$growth[wgy$tree == i] <- lead(wgy$growth[wgy$tree == i])}
+for(i in 1:10){wgy$growth[wgy$tree == i] <- lead(wgy$growth[wgy$tree == i])}
 ggplot(wgy, aes(x = w, y = growth, color = as.factor(tree))) +geom_point() + geom_smooth(method = "lm", se = FALSE)
 
+#convert back to wide for the model
+wgy2 <- subset(wgy, select = - c(w))
+g2 <- spread(wgy2, tree, growth)
 
 #create a model that can detect which trees are offset
-
 
 sink("model.txt")
 cat("   
     model{   
     
     for(t in 1:30){
-    for(y in 2:99){
+    for(y in 3:97){
       growth[y,t] ~ dnorm(mu[y,t], tau)
       mu[y,t] <- alpha * w[y + offset[t]]
     }
@@ -76,9 +78,9 @@ cat("
     sigma ~ dunif(0,10)
     alpha ~ dnorm(0, 0.001)
 
-    for(i in 1:30){offset[i] ~ dbern(0.5) 
-    }
+    for(i in 1:30){
 
+offset[i] ~ dbern(0.5)}
     }
     ",fill=TRUE)
 sink() 
@@ -86,13 +88,13 @@ sink()
 
 jags <- jags.model('model.txt',
                    data = list(                     
-                     'growth' = as.matrix(g),
+                     'growth' = g2,  #g2 has the offset data 
                      'w' = w),
                    n.chains = 3,
                    n.adapt = 100)
 
-mcmc_samples <- coda.samples(jags, variable.names=c("sigma","alpha", "offset"),  n.iter= 1000)
+mcmc_samples <- coda.samples(jags, variable.names=c("sigma","alpha", "offset"),  n.iter= 500)
 
-plot(mcmc_samples)
+#plot(mcmc_samples)
 resultsall <- summary(mcmc_samples) 
 results <- data.frame(resultsall$statistics,resultsall$quantiles) 
